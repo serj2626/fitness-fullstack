ec621b211cdde54791a298e5c160f1f2a0575361
serj2626 <serj2626@mail.ru>
2026-04-19 22:21:44 +0300

    updated interface

--- frontend/types/coach/coach.ts
+++ frontend/types/coach/coach.ts
@@ -1,25 +1,30 @@
+import type { IPagination } from "../common/pagination";
+
+export interface ICoachesListResponse extends IPagination {
+  results: ICoach[];
+}
 
 export interface ICoach {
-    id:         string;
-    first_name: string;
-    avatar:     string | null;
-    last_name:  string;
-    experience: number;
-    email:      string;
-    phone:      string;
-    categories: ICoachCategory[];
-    services:   ICoachService[];
+  id: string;
+  first_name: string;
+  avatar: string | null;
+  last_name: string;
+  experience: number | null;
+  email: string;
+  phone: string;
+  categories: ICoachCategory[];
+  services: ICoachService[];
 }
 
 export interface ICoachCategory {
-    id:   number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface ICoachService {
-    id:    number;
-    title: string;
-    price: number | null;
-    time:  number;
+  id: number;
+  title: string;
+  price: number | null;
+  time: number;
 }
